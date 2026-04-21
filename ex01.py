c674f4c9d5c7385f97ca3626e48c7eabd3e4e818
Samuel Cardoso <samuelcardoso22@hotmail.com>
2026-04-21 14:34:59 -0300

    Antecessor e Sucessor Número / Média de Notas

--- ex01.py
+++ ex01.py
@@ -1,8 +1,11 @@
-# num = int(input('Informe o numero:\n'))
-# print(f'Antecessor: {num - 1} | Numero: {num} | Sucessor: {num + 1}')
+def antecessor_sucesspor():
+    num = int(input('Informe o numero:\n'))
+    print(f'Antecessor: {num - 1} | Numero: {num} | Sucessor: {num + 1}')
 
-notas = []
+# -----------------------------------------------------------
+
 def calcular_media():
+    notas = []
     while len(notas) < 4:
         nota = float(input('Informe a nota:\n'))
         notas.append(nota)
@@ -10,4 +13,6 @@ def calcular_media():
     media = sum(notas) / len(notas)
     print(f'Media: {media}')
 
+# Escolha qual rodar
 calcular_media()
+# antecessor_sucesspor()
